36ab49121eee647fd309116510a6f80b3cdcf880
vsvad <47472249+vsvad@users.noreply.github.com>
2020-12-18 18:04:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,10 @@ import os
 from urllib.parse import urljoin
 from random import choice
 
-import aiohttp
-from aiohttp import web
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import context
-from aiogram.dispatcher.webhook import get_new_configured_app
 
 TOKEN = os.getenv('TOKEN', '')  # Press "Reveal Config Vars" in settings tab on Heroku and set TOKEN variable
-PROJECT_NAME = os.getenv('PROJECT_NAME', 'aiogram-example')  # Set it as you've set TOKEN env var
+PROJECT_NAME = os.getenv('PROJECT_NAME', 'matyama')  # Set it as you've set TOKEN env var
 
 WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com/'  # Enter here your link from Heroku project settings
 WEBHOOK_URL_PATH = '/webhook/' + TOKEN
